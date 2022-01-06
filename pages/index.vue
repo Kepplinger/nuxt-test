9e99b5aff8fdcf7b0d0fbec2adc831ce123b4a4e
Simon Kepplinger <simon.kepplinger@gmail.com>
2022-01-06 15:17:11 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Test yeah</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
+
+<style>
+  h1 {
+    margin-top: 200px;
+    text-align: center;
+    font-family: sans-serif;
+  }
+</style>
